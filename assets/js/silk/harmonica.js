3da667b077a71c1cfba38862be2251cd4c5c68ee
Bryan Stoner <bryan.stoner@idfive.com>
2016-06-07 22:34:13 -0400

    harmonicas

--- assets/js/silk/harmonica.js
+++ assets/js/silk/harmonica.js
@@ -7,13 +7,23 @@ function harmonica(parameters) {
   if(document.body.contains(container)) {
 
     for (var i = 0; i < headings.length; i++) {
+      var headingOpenIcon = document.createElement('svg');
+      headingOpenIcon.classList.add('symbol', 'symbol-plus');
+      headingOpenIcon.innerHTML = '<use xlink:href="plus"></use>';
+      headings[i].appendChild(headingOpenIcon);
+
+      var headingCloseIcon = document.createElement('svg');
+      headingCloseIcon.classList.add('symbol', 'symbol-minus');
+      headingCloseIcon.innerHTML = '<use xlink:href="plus"></use>';
+      headings[i].appendChild(headingCloseIcon);
+
       headings[i].addEventListener('click', toggleNote, false);
     }
 
     headings[0].click();
 
   }
-  
+
   function clearClasses() {
 
     for (var i = 0; i < headings.length; i++) {
