0aa8f2c6071d27c6c071110fd42388b5b29a9020
Steve Stephenson <hello@stevenstephenson.com>
2016-09-03 20:44:37 -0400

    Adds auto cycle

--- assets/js/silk/swift.js
+++ assets/js/silk/swift.js
@@ -3,131 +3,162 @@
  * v2.0.0
  */
 
-	'use strict';
-	var Swift = function (parameters) {
+'use strict';
+var Swift = function (parameters) {
+	this.container = document.querySelector(parameters.container);
+	this.elements = document.querySelectorAll(parameters.container + ' ' + parameters.elements);
+	this.elCount = this.elements.length;
+	this.currentSlide = 0;
+	this.cycle = parameters.cycle || false;
+	this.isPaused = this.cycle ? false : true;
+	this.interval = parameters.interval || 4000;
+	this.pages = [];
+	this.xDown = null;
+	this.yDown = null;
+	this.prevSymbol = parameters.prevSymbol;
+	this.nextSymbol = parameters.nextSymbol;
 
-		this.container = document.querySelector(parameters.container);
-		this.elements = document.querySelectorAll(parameters.container + ' ' + parameters.elements);
-		this.currentSlide = 0;
-		this.cycle = parameters.cycle || false;
-		this.paused = null;
-		this.interval = parameters.interval || 5000;
-		this.pages = [];
-		this.xDown = null;
-		this.yDown = null;
-		this.prevSymbol = parameters.prevSymbol;
-		this.nextSymbol = parameters.nextSymbol;
+	this.initialize.apply(this, parameters);
+};
 
-		this.initialize.apply(this, parameters);
-	};
+Swift.VERSION = '2.0.0';
 
-	Swift.VERSION = '2.0.0';
+Swift.prototype.initialize = function () {
+	this.controls();
+	this.pager();
+	this.autoCycle();
+};
 
-	Swift.prototype.initialize = function () {
-		this.controls();
-		this.pager();
+Swift.prototype.autoCycle = function () {
+	var self = this;
 
-	};
-
-	Swift.prototype.handleTouchStart = function (event) {
-		this.xDown = event.touches[0].clientX;
-		this.yDown = event.touches[0].clientY;
-	};
-
-	Swift.prototype.touchMoveHandler = function (event) {
-		var self = this;
-		if (!this.xDown || !this.yDown) {
+	if (!self.cycle ) {
+		return;
+	}
+	setInterval(function () {
+		if (self.isPaused ) {
 			return;
 		}
+		self.nextSlide(self.currentSlide);
+	}, self.interval);
 
-		var xUp = event.touches[0].clientX;
-		var yUp = event.touches[0].clientY;
+	this.pauseCycle();
+};
 
-		var xDiff = this.xDown - xUp;
-		var yDiff = this.yDown - yUp;
+Swift.prototype.pauseCycle = function (id) {
+	var self = this;
+	this.container.addEventListener('mouseover', function () {
+		self.isPaused = true;
+	});
+	this.container.addEventListener('mouseout', function () {
+		self.isPaused = false;
+	});
+};
 
-		if (Math.abs(xDiff) > Math.abs(yDiff)) {
-			if (xDiff > 0) {
-				this.nextSlide();
-			} else {
-				this.previousSlide();
-			}
+Swift.prototype.handleTouchStart = function (event) {
+	this.xDown = event.touches[0].clientX;
+	this.yDown = event.touches[0].clientY;
+};
+
+Swift.prototype.touchMoveHandler = function (event) {
+
+	if (!this.xDown || !this.yDown) {
+		return;
+	}
+
+	var xUp = event.touches[0].clientX;
+	var yUp = event.touches[0].clientY;
+
+	var xDiff = this.xDown - xUp;
+	var yDiff = this.yDown - yUp;
+
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		if (xDiff > 0) {
+			this.nextSlide();
+		} else {
+			this.previousSlide();
 		}
+	}
 
-		/* reset values */
-		this.xDown = null;
-		this.yDown = null;
+	/* reset values */
+	this.xDown = null;
+	this.yDown = null;
 
-	};
+};
 
-	Swift.prototype.clearClasses = function () {
-		for (var i = 0; i < this.elements.length; i++) {
-			this.pages[i].classList.remove('active');
-			this.elements[i].classList.remove('active');
-		}
-	};
 
-	Swift.prototype.assignClasses = function (index) {
-		this.pages[index].classList.add('active');
-		this.elements[index].classList.add('active');
-	};
+Swift.prototype.clearClasses = function () {
+	for (var i = 0; i < this.elements.length; i++) {
+		this.pages[i].classList.remove('active');
+		this.elements[i].classList.remove('active');
+	}
+};
 
-	Swift.prototype.pager = function () {
+Swift.prototype.assignClasses = function (index) {
+	this.pages[index].classList.add('active');
+	this.elements[index].classList.add('active');
+};
 
-		var pager = document.createElement('div');
-		pager.classList.add('swift-pager');
+Swift.prototype.pager = function () {
 
-		this.container.appendChild(pager);
+	var pager = document.createElement('div');
+	pager.classList.add('swift-pager');
 
-		for (var i = 0; i < this.elements.length; i++) {
-			this.pages.push(document.createElement('span'));
-			pager.appendChild(this.pages[i]);
-			this.pages[i].addEventListener('click', this.slide.bind(this, i), false);
-		}
+	this.container.appendChild(pager);
 
-		this.pages[this.currentSlide].click();
-	};
+	for (var i = 0; i < this.elements.length; i++) {
+		this.pages.push(document.createElement('span'));
+		pager.appendChild(this.pages[i]);
+		this.pages[i].addEventListener('click', this.slide.bind(this, i), false);
+	}
 
-	Swift.prototype.slide = function (index) {
-		this.clearClasses();
-		this.assignClasses(index);
-	};
+	this.pages[this.currentSlide].click();
+};
 
-	Swift.prototype.nextSlide = function () {
-		if (this.currentSlide == this.elements.length - 1) {
-			this.currentSlide = -1;
-		}
-		this.currentSlide = this.currentSlide + 1;
-		this.slide(this.currentSlide);
-	};
+Swift.prototype.slide = function (index) {
+	this.currentSlide = index;
+	this.clearClasses();
+	this.assignClasses(index);
+};
 
-	Swift.prototype.previousSlide = function () {
-		if (this.currentSlide === 0) {
-			this.currentSlide = elements.length;
-		}
-		this.currentSlide = this.currentSlide - 1;
-		this.slide(this.currentSlide);
-	};
+Swift.prototype.nextSlide = function () {
 
-	Swift.prototype.controls = function () {
-		var controller = document.createElement('div');
-		var prev = document.createElement('button');
-		var next = document.createElement('button');
-		// Controls Container
-		controller.classList.add('swift-controls');
-		this.container.appendChild(controller);
-		// Prev
-		prev.addEventListener('click', this.previousSlide.bind(this), false);
-		prev.classList.add('swift-control', 'swift-prev');
-		prev.innerHTML = '<svg class="symbol symbol-' + this.prevSymbol + '"><use xlink:href="#' + this.prevSymbol + '"></use></svg>';
-		// Next
-		next.addEventListener('click', this.nextSlide.bind(this), false);
-		next.classList.add('swift-control', 'swift-next');
-		next.innerHTML = '<svg class="symbol symbol-' + this.nextSymbol + '"><use xlink:href="#' + this.nextSymbol + '"></use></svg>';
+	if (this.currentSlide == this.elements.length - 1) {
+		this.currentSlide = -1;
+	}
 
-		controller.appendChild(prev);
-		controller.appendChild(next);
-	};
+	this.currentSlide = this.currentSlide + 1;
+	this.slide(this.currentSlide);
+};
+
+Swift.prototype.previousSlide = function () {
+	if (this.currentSlide === 0) {
+		this.currentSlide = elements.length;
+	}
+
+	this.currentSlide = this.currentSlide - 1;
+	this.slide(this.currentSlide);
+};
+
+Swift.prototype.controls = function () {
+	var controller = document.createElement('div');
+	var prev = document.createElement('button');
+	var next = document.createElement('button');
+	// Controls Container
+	controller.classList.add('swift-controls');
+	this.container.appendChild(controller);
+	// Prev
+	prev.addEventListener('click', this.previousSlide.bind(this), false);
+	prev.classList.add('swift-control', 'swift-prev');
+	prev.innerHTML = '<svg class="symbol symbol-' + this.prevSymbol + '"><use xlink:href="#' + this.prevSymbol + '"></use></svg>';
+	// Next
+	next.addEventListener('click', this.nextSlide.bind(this), false);
+	next.classList.add('swift-control', 'swift-next');
+	next.innerHTML = '<svg class="symbol symbol-' + this.nextSymbol + '"><use xlink:href="#' + this.nextSymbol + '"></use></svg>';
+
+	controller.appendChild(prev);
+	controller.appendChild(next);
+};
 
 
 
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,108 +1,109 @@
 /* ========================================
-// Social Configurations
-// ===================================== */
+ // Social Configurations
+ // ===================================== */
 
 var twitterConfig = {
-  id: '347099293930377217',
-  domId: 'twitter-feed',
-  maxTweets: 1,
-  enableLinks: true,
-  showInteraction: false,
-  showUser: false,
-  showTime: false,
-  lang: 'en',
-  customCallback: twitterTemplate,
+	id: '347099293930377217',
+	domId: 'twitter-feed',
+	maxTweets: 1,
+	enableLinks: true,
+	showInteraction: false,
+	showUser: false,
+	showTime: false,
+	lang: 'en',
+	customCallback: twitterTemplate,
 }
 
 
 /* ========================================
-// Site Functions
-// ===================================== */
+ // Site Functions
+ // ===================================== */
 
 init();
 
 function init() {
 
-  silkNav();
-  hero();
-  harmonicas();
-  silkModal();
-  slideshows();
-  triggers();
-  twitter();
+	silkNav();
+	hero();
+	harmonicas();
+	silkModal();
+	slideshows();
+	triggers();
+	twitter();
 
 }
 
 function harmonicas() {
 
-  harmonica({
-    container: '.silk-harmonica--condensed',
-    header: '.silk-harmonica__header'
-  });
+	harmonica({
+		container: '.silk-harmonica--condensed',
+		header: '.silk-harmonica__header'
+	});
 
-  harmonica({
-    container: '.silk-harmonica--expanded',
-    header: '.silk-harmonica__header'
-  });
+	harmonica({
+		container: '.silk-harmonica--expanded',
+		header: '.silk-harmonica__header'
+	});
 
 }
 
 function slideshows() {
 
-  var swiftSlide = document.querySelectorAll('.swift-slide');
+	var swiftSlide = document.querySelectorAll('.swift-slide');
 
-  if(document.body.contains(swiftSlide[0])) {
+	if (document.body.contains(swiftSlide[0])) {
 
-    for(var i = 0; i < swiftSlide.length; i++) {
-      swiftSlide[i].classList.add('swift-slide-' + i);
+		for (var i = 0; i < swiftSlide.length; i++) {
+			swiftSlide[i].classList.add('swift-slide-' + i);
 
-      new Swift({
-        container: '.swift-slide-' + i,
-        elements: 'li',
-        prevSymbol: 'chevron-left',
-        nextSymbol: 'chevron-right'
-      });
+			new Swift({
+				container: '.swift-slide-' + i,
+				elements: 'li',
+				prevSymbol: 'chevron-left',
+				nextSymbol: 'chevron-right',
+				cycle: false
+			});
 
-    }
+		}
 
-  }
+	}
 
 }
 
 function triggers() {
 
-  triggerParent({
-    trigger: '.site-search__trigger'
-  });
+	triggerParent({
+		trigger: '.site-search__trigger'
+	});
 
 }
 
 function halfways() {
 
-  halfway({
-    element: '.halfway-section'
-  });
+	halfway({
+		element: '.halfway-section'
+	});
 
 }
 
 function twitter() {
 
-  var twitterFeed = document.querySelector('#twitter-feed');
+	var twitterFeed = document.querySelector('#twitter-feed');
 
-  if(document.body.contains(twitterFeed)) {
-    twitterFetcher.fetch(twitterConfig);
-  }
+	if (document.body.contains(twitterFeed)) {
+		twitterFetcher.fetch(twitterConfig);
+	}
 
 }
 
 function twitterTemplate(tweets) {
 
-  var element = document.getElementById('twitter-feed');
-  var i = 0;
+	var element = document.getElementById('twitter-feed');
+	var i = 0;
 
-  while(i < tweets.length) {
-    element.innerHTML += '<li class="twitter-item">' + tweets[i] + '</li>';
-    i++;
-  }
+	while (i < tweets.length) {
+		element.innerHTML += '<li class="twitter-item">' + tweets[i] + '</li>';
+		i++;
+	}
 
 }
